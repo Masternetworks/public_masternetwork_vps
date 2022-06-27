512b2c5492241ea2344156eb1a95c2bda3b61a81
Masternetworks <parmicciano@protonmail.com>
2022-06-27 11:55:36 +0200

    node installer

--- node_installer.sh
+++ node_installer.sh
@@ -1,4 +1,4 @@
-#git clone https://github.com/Masternetworks/masternetwork_client.git
+git clone https://github.com/Masternetworks/masternetwork_client.git
 cd masternetwork_client
 while true
 do
@@ -6,12 +6,12 @@ GP=$(git pull)
 
 if [[ $GP = "Already up to date." ]]; then
     
-    sudo ./x86_linux &
+    sudo ./86_linux -apikey MYKEYFROMmasternetwork.api -ram 8000 -vcpus 2 -storage 85 -nodename Sunlight &
     MyPID=$!                  
     sleep 2m 30s
 else
 kill $MyPID 
-sudo ./x86_linux &
+sudo ./86_linux -apikey MYKEYFROMmasternetwork.api -ram 8000 -vcpus 2 -storage 85 -nodename Sunlight &
 MyPID=$!
 echo "UPDATED DONE"
 fi
