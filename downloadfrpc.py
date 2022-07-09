384fe3d25894cba277c55ab329c518ad335d907b
Masternetworks <104528187+Masternetworks@users.noreply.github.com>
2022-07-09 13:55:00 +0200

    Update downloadfrpc.py

--- downloadfrpc.py
+++ downloadfrpc.py
@@ -1,20 +1,18 @@
 import requests, os, sys, time 
 from requests.exceptions import HTTPError
 
-while True :
-    response = requests.get('https://api.masternetwork.dev/FRPCdemand/'+ sys.argv[1])
-    response.raise_for_status()
-    # access JSOn content
-    jsonResponse = response.json()
-    content = jsonResponse["content"]
-    if content == "nothing":
-            time.sleep(180)
-    else :
-        file_path = "/var/mydir/frp/frp_0.42.0_linux_amd64/frpc.ini"
-        os.remove(file_path)
-        f = open(file_path, "w")
-        f.write(content)
-        f.close()
-        os.system("cd /var/mydir/frp/frp_0.42.0_linux_amd64/ && ./frpc -c ./frpc.ini")
-        time.sleep(5)
+response = requests.get('https://api.masternetwork.dev/FRPCdemand/'+ sys.argv[1])
+response.raise_for_status()
+jsonResponse = response.json()
+content = jsonResponse["content"]
+if content == "nothing":
+        time.sleep(180)
+else :
+    file_path = "/var/mydir/frp/frp_0.42.0_linux_amd64/frpc.ini"
+    os.remove(file_path)
+    f = open(file_path, "w")
+    f.write(content)
+    f.close()
+    os.system("cd /var/mydir/frp/frp_0.42.0_linux_amd64/ && ./frpc -c ./frpc.ini")
+    time.sleep(60)
 
