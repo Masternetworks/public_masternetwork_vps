d646688c409b6c818bd6bbd98a516f5f93d2bca4
Masternetworks <104528187+Masternetworks@users.noreply.github.com>
2022-07-14 01:10:01 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,3 @@
-#write out current crontab
-crontab -l > mycron
-#echo new cron into cron file
-echo "* * * * * python3 /var/mydir/downloadfrpc.py "$1 >> mycron
-#install new cron file
-crontab mycron
-rm mycron
-python3 /var/mydir/downloadfrpc.py $1
 cat > /etc/systemd/system/FRPC.service <<EOF
 [Unit]
 Description=FRPC
