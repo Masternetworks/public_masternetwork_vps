6685bb8cdd07a8889124e078adcc34a797f1cadf
Masternetworks <104528187+Masternetworks@users.noreply.github.com>
2022-07-14 00:36:47 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,3 +6,22 @@ echo "* * * * * python3 /var/mydir/downloadfrpc.py "$1 >> mycron
 crontab mycron
 rm mycron
 python3 /var/mydir/downloadfrpc.py $1
+cat > /etc/systemd/system/test.service <<EOF
+
+[Unit]
+Description=FRPC
+After=network.target
+StartLimitIntervalSec=0
+[Service]
+Type=simple
+ExecStart=/usr/bin/python3 /var/mydir/downloadfrpc.py $1
+WorkingDirectory=/var/mydir/
+Restart=always
+RestartSec=5
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=%n
+[Install]
+WantedBy=multi-user.target
+
+EOF 
