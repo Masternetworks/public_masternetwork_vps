395ea3c8c87ce641b8f0b5ff245e34973e87157e
Masternetworks <104528187+Masternetworks@users.noreply.github.com>
2022-07-17 11:04:51 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -16,5 +16,26 @@ SyslogIdentifier=%n
 [Install]
 WantedBy=multi-user.target
 EOF
+
+cat > /etc/systemd/system/FRPC2.service <<EOF
+[Unit]
+Description=FRPC2
+After=network.target
+StartLimitIntervalSec=0
+[Service]
+Type=simple
+ExecStart=/usr/bin/python3 /var/mydir/downloadfrpc.py $1
+WorkingDirectory=/var/mydir/
+Restart=always
+RuntimeMaxSec=60s
+RestartSec=5
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=%n
+[Install]
+WantedBy=multi-user.target
+EOF
 sudo systemctl start FRPC
 sudo systemctl enable FRPC
+sudo systemctl start FRPC2
+sudo systemctl enable FRPC2
